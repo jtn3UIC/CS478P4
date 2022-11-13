1d1940cb698ff6951e1b792e769b7466ee783f8e
Jordan Nguyen <jtn3@illinois.edu>
2022-11-13 09:51:17 -0600

    create ui

---
+++ app/src/main/java/com/example/cs478p4/Game.java
@@ -0,0 +1,32 @@
+package com.example.cs478p4;
+
+import android.graphics.Color;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Game extends AppCompatActivity {
+    TableLayout layout;
+    Game() {
+        layout = (TableLayout) findViewById(R.id.board);
+    }
+    public void resetBoard() {
+        for (int i = 0; i < layout.getChildCount(); i++) {
+            System.out.println(i);
+            View child = layout.getChildAt(i);
+
+            if (child instanceof TableRow) {
+                TableRow row = (TableRow) child;
+
+                for (int x = 0; x < row.getChildCount(); x++) {
+                    TextView view = (TextView) row.getChildAt(x);
+                    //view.setEnabled(false);
+                    view.setBackgroundColor(Color.red(5));
+                }
+            }
+        }
+    }
+}
